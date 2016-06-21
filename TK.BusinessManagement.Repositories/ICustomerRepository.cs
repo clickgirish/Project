664d879e70d463656c887721c4bb0a1151449f60
Girish Chawrai <clickgirish@hotmail.com>
2016-06-21 11:54:36 +0530

    Reference updates and customer repository.

---
+++ TK.BusinessManagement.Repositories/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TK.BusinessManagement.Models;
+
+namespace TK.BusinessManagement.Repositories
+{
+    public interface ICustomerRepository
+    {
+        bool SaveCustomer(CustomerModel customer);
+        bool DeleteCustomer(CustomerModel customer);
+    }
+}
